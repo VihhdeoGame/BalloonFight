49cf2d1661220d49eac651f214b513238906c3af
Victor <Victorosario1@hotmail.com>
2021-09-03 11:01:00 -0300

    Weapon move

--- BallonFight/Assets/Scripts/WeaponMove.cs
+++ BallonFight/Assets/Scripts/WeaponMove.cs
@@ -33,13 +33,13 @@ public class WeaponMove : MonoBehaviour
         float shieldY = -Mathf.Cos((rAngle)*Mathf.Deg2Rad)*distance; 
         shield.transform.localPosition = new Vector3(shieldX,shieldY,0);
         
-        Vector3 swordVector = sword.transform.position - player.transform.position;
-        float swordA = Mathf.Atan2(swordVector.x, swordVector.y) * Mathf.Rad2Deg;
-        sword.transform.rotation = Quaternion.Euler(0,0,-swordA);
+        Vector3 swordVector = player.transform.position - sword.transform.position;
+        float swordA = -Mathf.Atan2(swordVector.x, swordVector.y) * Mathf.Rad2Deg;
+        sword.transform.rotation = Quaternion.Euler(0,0,swordA);
         
-        Vector3 shieldVector = shield.transform.position - player.transform.position;
-        float shieldA = Mathf.Atan2(swordVector.x, swordVector.y) * Mathf.Rad2Deg;
-        shield.transform.rotation = Quaternion.Euler(0,0,-shieldA);
+        Vector3 shieldVector = player.transform.position - shield.transform.position;
+        float shieldA = -Mathf.Atan2(swordVector.x, swordVector.y) * Mathf.Rad2Deg;
+        shield.transform.rotation = Quaternion.Euler(0,0,shieldA);
         
     }
     void Rotate(bool dirRight)
@@ -52,20 +52,6 @@ public class WeaponMove : MonoBehaviour
         {
             rAngle++;
         }
-        float swordX = Mathf.Sin(rAngle*Mathf.Deg2Rad)*distance;
-        float swordY = Mathf.Cos(rAngle*Mathf.Deg2Rad)*distance; 
-        sword.transform.localPosition = new Vector3(swordX,swordY,0);
-
-        float shieldX = -Mathf.Sin((rAngle)*Mathf.Deg2Rad)*distance;
-        float shieldY = -Mathf.Cos((rAngle)*Mathf.Deg2Rad)*distance; 
-        shield.transform.localPosition = new Vector3(shieldX,shieldY,0);
-
-        Vector3 swordVector = sword.transform.position - player.transform.position;
-        float swordA = Mathf.Atan2(swordVector.x, swordVector.y) * Mathf.Rad2Deg;
-        sword.transform.rotation = Quaternion.Euler(0,0,-swordA);
-        
-        Vector3 shieldVector = shield.transform.position - player.transform.position;
-        float shieldA = Mathf.Atan2(swordVector.x, swordVector.y) * Mathf.Rad2Deg;
-        shield.transform.rotation = Quaternion.Euler(0,0,-shieldA);
+        Rotate();
     }
 }
